fb4e3a4346b1d3d4f7038352967d1dc31887d592
Sashko Stubailo <sashko@mit.edu>
2014-12-03 23:46:08 -0800

    Imply the reload package so that Reload is accessible

--- packages/mdg:reload-on-resume/package.js
+++ packages/mdg:reload-on-resume/package.js
@@ -1,13 +1,17 @@
 Package.describe({
   name: "mdg:reload-on-resume",
   summary: "On Cordova, only allow the app to reload when the app is resumed.",
-  version: '1.0.1',
+  version: '1.0.2',
   git: "https://github.com/meteor/mobile-packages"
 });
 
 Package.on_use(function (api) {
   api.versionsFrom("0.9.2");
-  api.use(['reload', 'reactive-var'], 'web');
+  api.use(['reactive-var'], 'web');
+
+  // Imply so that we can access the Reload export from an app
+  api.imply(['reload'], 'web');
+
   api.add_files("reload-on-resume.js", 'web.cordova');
   api.add_files("browser.js", 'web.browser');
 });
