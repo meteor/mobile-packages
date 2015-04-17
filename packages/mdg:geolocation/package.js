0d1cc2a161a0988c8b3a3976c73ede401649fb27
Dan Dascalescu <ddascalescu+github@gmail.com>
2015-04-17 06:49:06 -0700

    Update for Meteor 0.9+

--- packages/mdg:geolocation/package.js
+++ packages/mdg:geolocation/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "mdg:geolocation",
   summary: "Provides reactive geolocation on desktop and mobile.",
-  version: "1.0.2",
+  version: "1.0.3",
   git: "https://github.com/meteor/mobile-packages"
 });
 
@@ -9,9 +9,9 @@ Cordova.depends({
   "org.apache.cordova.geolocation": "0.3.10"
 });
 
-Package.on_use(function (api) {
+Package.onUse(function (api) {
   api.use(["reactive-var"]);
   api.versionsFrom("METEOR@0.9.2");
-  api.add_files(["geolocation.js"], "client");
+  api.addFiles(["geolocation.js"], "client");
   api.export("Geolocation", "client");
 });
