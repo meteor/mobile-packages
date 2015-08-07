304860e9d85e5465dc345fc21010f66f10ab9f2b
Sashko Stubailo <sashko@stubailo.com>
2015-08-07 13:52:52 -0700

    Update LESS dep for Meteor 1.2

--- packages/mdg:camera/package.js
+++ packages/mdg:camera/package.js
@@ -11,7 +11,7 @@ Cordova.depends({
 
 Package.onUse(function(api) {
   api.export('MeteorCamera');
-  api.use(["templating", "session", "ui", "blaze", "less", "reactive-var"]);
+  api.use(["templating", "session", "ui", "blaze", "less@1.0.0||2.0.0", "reactive-var"]);
   api.versionsFrom("METEOR@0.9.2");
 
   api.addFiles('photo.html');
