ef2ca2eee1eb3ce4db84ae15c7dd2e795d2060f9
Martijn Walraven <martijn@martijnwalraven.com>
2015-09-21 22:36:03 +0200

    Update plugin dependencies and examples for Meteor 1.2

--- packages/mdg:geolocation/package.js
+++ packages/mdg:geolocation/package.js
@@ -1,17 +1,18 @@
 Package.describe({
   name: "mdg:geolocation",
   summary: "Provides reactive geolocation on desktop and mobile.",
-  version: "1.0.3",
+  version: "1.1.0",
   git: "https://github.com/meteor/mobile-packages"
 });
 
 Cordova.depends({
-  "org.apache.cordova.geolocation": "0.3.10"
+  "cordova-plugin-geolocation": "1.0.1"
 });
 
 Package.on_use(function (api) {
   api.use(["reactive-var"]);
-  api.versionsFrom("METEOR@0.9.2");
+  api.versionsFrom("METEOR@1.2");
+  api.use("isobuild:cordova@5.2.0");
   api.add_files(["geolocation.js"], "client");
   api.export("Geolocation", "client");
 });
