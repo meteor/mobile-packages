200e252b4bc487006b43bf4fbe1bb2aae13a38e8
Petar Korponaić <petar.korponaic@gmail.com>
2016-02-16 00:52:54 +0100

    Moved cordova back to 5.2.0 and fixed 'MediaStream.stop()' deprecated in Chrome

--- packages/mdg:camera/package.js
+++ packages/mdg:camera/package.js
@@ -13,7 +13,7 @@ Package.onUse(function(api) {
   api.export('MeteorCamera');
   api.use(["templating", "session", "ui", "blaze", "less@1.0.0||2.0.0", "reactive-var"]);
   api.versionsFrom("METEOR@1.2");
-  api.use("isobuild:cordova@5.4.0");
+  api.use("isobuild:cordova@5.2.0");
 
   api.addFiles('photo.html');
   api.addFiles('photo.js');
--- packages/mdg:camera/photo-browser.js
+++ packages/mdg:camera/photo-browser.js
@@ -84,6 +84,28 @@ var browserNotSupportedError = function () {
   return error.get() && error.get() === "BROWSER_NOT_SUPPORTED";
 };
 
+var stopStream = function(st) {
+  if(!st) {
+    return;
+  }
+
+  if(st.stop) {
+    st.stop();
+    return;
+  }
+
+  if(st.getTracks) {
+    var tracks = st.getTracks();
+    for(var i = 0; i < tracks.length; i++) {
+      var track = tracks[i];
+      if(track && track.stop) {
+        track.stop();
+      }
+    }
+  }
+};
+
+
 Template.camera.helpers({
   photo: function () {
     return photo.get();
@@ -114,7 +136,7 @@ Template.camera.events({
     }
     
     if (stream) {
-      stream.stop();
+      stopStream(stream);
     }
   }
 });
@@ -129,7 +151,7 @@ Template.viewfinder.events({
     canvas.getContext('2d').drawImage(video, 0, 0, canvasWidth, canvasHeight);
     var data = canvas.toDataURL('image/jpeg', quality);
     photo.set(data);
-    stream.stop();
+    stopStream(stream);
   }
 });
 
