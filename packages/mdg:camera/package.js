9a1d1a5b77db8f6a0b1f1f85131324fdb3969f36
Jan Dvorak <storyteller@freedombase.net>
2021-10-05 20:24:44 +0200

    Update cordova camera to v6

--- packages/mdg:camera/package.js
+++ packages/mdg:camera/package.js
@@ -1,17 +1,17 @@
 Package.describe({
   name: "mdg:camera",
   summary: "Photos with one function call on desktop and mobile.",
-  version: "2.0.0",
+  version: "1.7.0",
   git: "https://github.com/meteor/mobile-packages"
 });
 
 Cordova.depends({
-  "cordova-plugin-camera": "4.0.3"
+  "cordova-plugin-camera": "6.0.0"
 });
 
 Package.onUse(function(api) {
 
-  api.versionsFrom("2.3.6");
+  api.versionsFrom("2.5");
   api.export('MeteorCamera');
   api.use(["templating@1.4.1", "session@1.2.0", "blaze@2.5.0", "less@4.0.0", "reactive-var@1.0.11"]);
   api.use("isobuild:cordova@5.2.0");
