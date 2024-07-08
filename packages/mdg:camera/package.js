bf0419aca10d13157a66431c5dd11c65e3c41977
Jan Dvorak <storytellercz@proton.me>
2024-07-08 17:23:23 +0200

    Update cordova-plugin-camera to v7 & versions to Meteor 3

--- packages/mdg:camera/package.js
+++ packages/mdg:camera/package.js
@@ -1,19 +1,19 @@
 Package.describe({
   name: "mdg:camera",
   summary: "Photos with one function call on desktop and mobile.",
-  version: "1.8.0",
+  version: "1.9.0",
   git: "https://github.com/meteor/mobile-packages"
 });
 
 Cordova.depends({
-  "cordova-plugin-camera": "6.0.0"
+  "cordova-plugin-camera": "7.0.0"
 });
 
 Package.onUse(function(api) {
 
-  api.versionsFrom("2.5");
+  api.versionsFrom(["2.5", "3.0"]);
   api.export('MeteorCamera');
-  api.use(["templating@1.4.1", "session@1.2.0", "blaze@2.5.0", "less@4.0.0", "reactive-var@1.0.11"]);
+  api.use(["templating", "session", "blaze", "less", "reactive-var"]);
   api.use("isobuild:cordova@5.2.0");
 
   api.addFiles('photo.html');
